79935798b92f69cc630924c35c0f2bbed6a5737a
Mahdi <mahdiprog81@gmail.com>
2025-07-21 18:10:27 +0330

    change port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser"); // Middleware to parse cookies
 
 const app = express();
-const PORT = 3001;
+const PORT = 3000;
 
 // Middleware to parse cookies
 app.use(cookieParser());
